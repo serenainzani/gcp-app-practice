fbee0ad62cd80c5ea484e602901dfe5e8820d06a
Serena <serenainzani@hotmail.com>
2022-06-16 21:42:23 +0100

    basic app structure

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get('/boo', (req, res) => {
+  res.send('Boo!')
+})
+
+app.use(function(req, res, next) {
+  return res.status(404).send('Route '+req.url+' Not found.');
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
