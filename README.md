24c0d3dafa4da2eb63e50f238ac328f5cc318f5b
Serena <serenainzani@hotmail.com>
2022-06-16 22:16:26 +0100

    file updates for gcp deploy

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # gcp-app-practice
+
+## Running this app
+
+1. install node modules
+``` bash
+npm install
+```
+
+2. deploy to cloud
+``` bash
+gcloud init
--- app.js
+++ app.js
@@ -5,16 +5,17 @@ const port = parseInt(process.env.PORT) || 3000;
 app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  // res.sendFile(__dirname + "/index.html");
+  res.send('Hello world!')
 });
 
 app.get('/boo', (req, res) => {
   res.send('Boo!')
 })
 
-app.use(function(req, res, next) {
-  return res.status(404).send('Route '+req.url+' Not found.');
-});
+// app.use(function(req, res, next) {
+//   return res.status(404).send('Route '+req.url+' Not found.');
+// });
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
